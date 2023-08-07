cdb26e766c4e64ace84f97d7e6bb92a04e76f97d
andrecruzmendes <andrecruzmendes@gmail.com>
2023-08-07 08:52:21 +0000

    atualização do Érdna

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Thays, eu te amo. Se você também me ama, dá uma risadinha!</h1>
+    return <h1>Estou aqui para ajudar no curso.dev! 🤖</h1>
 }
 
 export default Home;
