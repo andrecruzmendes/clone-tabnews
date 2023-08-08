a2fe6f4c325e9ff28c3757c88150abd3da6806de
andrecruzmendes <andrecruzmendes@gmail.com>
2023-08-08 09:18:08 +0000

    update prhase challenge

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Thays Amanda, eu amo você! Se você também me ama, dá uma risadinha 🥰</h1>
+    return <div>
+        <p>Os últimos 10 anos foram repletos de pequenos e grandes gestos seus que me cativaram todos os dias, dia após dia.</p>
+        <p>Em tudo o que você fez, pude ver seu coração cheio de vontade de nos dar alegria e felicidade.</p>
+        <p>Houve dias agradáveis e dias desafiadores, é verdade. Eu amei passar por todas essas fases ao seu lado.</p>
+        <p>Meu coração se enche de alegria e de orgulho de fazer parte da sua e da nossa jornada, obrigado por tudo!</p>
+        <p>Que venham muitos mais anos assim.</p>
+        <p><b>Amo você.</b></p>
+        <p>E se você também me ama, dá uma risadinha 🥰</p>
+        <p><i>Bora viajar?</i></p>
+    </div>
 }
 
 export default Home;
