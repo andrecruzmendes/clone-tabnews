a86937330f3e4e3675839d0e090290059f12c03e
André Cruz Mendes <andrecruzmendes@gmail.com>
2023-12-19 10:21:12 +0000

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -8,39 +8,18 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
-}
 
-async function getVersion() {
-  const result = await query("SELECT version()");
-  return result.rows[0].version;
-}
-async function getMaxConnections() {
-  const result = await query("SHOW max_connections");
-  return result.rows[0].max_connections;
-}
-async function getActiveConnections() {
-  const result = await query("SELECT count(*) FROM pg_stat_activity");
-  return result.rows[0].count;
-}
-async function status() {
+  let result = null;
   try {
-    return {
-      version: await getVersion(),
-      max_connections: await getMaxConnections(),
-      active_connections: await getActiveConnections(),
-    };
-  } catch (error) {
-    return {
-      errorCode: error.code,
-    };
+    await client.connect();
+    result = await client.query(queryObject);
+  } finally {
+    await client.end();
   }
+
+  return result;
 }
 
 export default {
   query: query,
-  status: status,
 };
