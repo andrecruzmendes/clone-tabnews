505b7e10071218c037eef8b78dc930e152620660
André Cruz Mendes <andrecruzmendes@gmail.com>
2023-12-19 10:21:59 +0000

    finalize `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -6,19 +6,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  if ("errorCode" in responseBody.database) {
-    expect(responseBody.database).toEqual(
-      expect.objectContaining({
-        errorCode: expect.any(String),
-      }),
-    );
-  } else {
-    expect(responseBody.database).toEqual(
-      expect.objectContaining({
-        version: expect.any(String),
-        max_connections: expect.stringMatching(/^\d+$/),
-        active_connections: expect.stringMatching(/^\d+$/),
-      }),
-    );
-  }
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
