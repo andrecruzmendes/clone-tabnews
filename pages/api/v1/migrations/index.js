53b76fc1015477431b775401aede00c7dba38a51
André Cruz Mendes <andrecruzmendes@gmail.com>
2024-05-03 10:50:50 +0000

    add json info to response error

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -17,7 +17,9 @@ function getMigrationOptions(dbClient, liveRun) {
 export default async function migrations(request, response) {
   const allowedMethods = ["GET", "POST"];
   if (!allowedMethods.includes(request.method)) {
-    return response.status(405).end();
+    return response.status(405).json({
+      error: `Method ${request.method} not allowed`,
+    });
   }
   let dbClient;
   try {
