617e5910aa854381f3cc4089b05163576e563cec
André Cruz Mendes <andrecruzmendes@gmail.com>
2024-09-30 15:28:44 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,21 +6,29 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const liveRunMigrations = async () =>
-    await fetch("http://localhost:3000/api/v1/migrations", {
-      method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      const liveRunMigrations = async () =>
+        await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+
+      test("For the first time", async () => {
+        const firstResponse = await liveRunMigrations();
+        expect(firstResponse.status).toBe(201);
+        const firstResponseBody = await firstResponse.json();
+        expect(Array.isArray(firstResponseBody)).toBe(true);
+        expect(firstResponseBody.length).toBeGreaterThan(0);
+      });
+
+      test("For the second time", async () => {
+        const secondResponse = await liveRunMigrations();
+        expect(secondResponse.status).toBe(200);
+        const secondResponseBody = await secondResponse.json();
+        expect(Array.isArray(secondResponseBody)).toBe(true);
+        expect(secondResponseBody.length).toBe(0);
+      });
     });
-
-  const firstResponse = await liveRunMigrations();
-  expect(firstResponse.status).toBe(201);
-  const firstResponseBody = await firstResponse.json();
-  expect(Array.isArray(firstResponseBody)).toBe(true);
-  expect(firstResponseBody.length).toBeGreaterThan(0);
-
-  const secondResponse = await liveRunMigrations();
-  expect(secondResponse.status).toBe(200);
-  const secondResponseBody = await secondResponse.json();
-  expect(Array.isArray(secondResponseBody)).toBe(true);
-  expect(secondResponseBody.length).toBe(0);
+  });
 });
